26231aaf8b36f1c16848aea46e586b8726d49bc5
Solovyev Sergey <solovyev@twinscom.ru>
2019-09-17 17:50:37 +0300

    035 Функциональные компоненты в классы

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 
-//TODO: end 033
+//TODO: end 034
 
 //------------------
 ReactDOM.render(<App />, document.getElementById('root'));
