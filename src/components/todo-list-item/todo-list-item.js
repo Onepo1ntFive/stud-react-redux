08988711b3a7d54c51edd6514ca13829b49ef904
SergeySolovev <name.diesel@gmail.com>
2019-09-18 07:23:27 +0300

    035 Добавление незавершенного click обработчика

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './todo-list-item.css'
 
 class TodoListItem extends Component {
+
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
+    }
+
+
     render() {
 
         const { label, important = false } = this.props;
@@ -16,6 +22,7 @@ class TodoListItem extends Component {
                 <span
                     className="todo-list-item-label"
                     style={style}
+                    onClick={this.onLabelClick}
                 >
                     {label}
                 </span>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 
-//TODO: end 034
+//TODO: end 035
 
 //------------------
 ReactDOM.render(<App />, document.getElementById('root'));
