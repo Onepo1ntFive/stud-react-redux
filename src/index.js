024628079d80a3951298662a16fb035865045b7d
SergeySolovev <name.diesel@gmail.com>
2019-09-25 07:51:01 +0300

    038 Добавление state и setState для элементов списка

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 
-//TODO: end 035
+//TODO: end 038
 
 //------------------
 ReactDOM.render(<App />, document.getElementById('root'));
